900d731f990021e6dd4e78bcd4a73c7b601c7cb8
unknown <saranvelusamy2005@gmail.com.com>
2026-04-17 16:47:24 +0530

    UC16: Implement Bubble Sort for bogie capacities

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -195,3 +195,35 @@ public class TrainConsistManagementApp {
         }
     }
 }
+// ---------- UC16: Bubble Sort ----------
+System.out.println("\nUC16: Bubble Sort (Manual Sorting)");
+
+        // Array of capacities
+        int[] capacities = {72, 56, 24, 70, 60};
+
+// Print before sorting
+System.out.println("Before Sorting:");
+for (int c : capacities) {
+        System.out.print(c + " ");
+}
+
+        // Bubble Sort logic
+        int n = capacities.length;
+
+for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+        if (capacities[j] > capacities[j + 1]) {
+
+        // Swap
+        int temp = capacities[j];
+        capacities[j] = capacities[j + 1];
+        capacities[j + 1] = temp;
+        }
+                }
+                }
+
+// Print after sorting
+                System.out.println("\nAfter Sorting:");
+for (int c : capacities) {
+        System.out.print(c + " ");
+}
